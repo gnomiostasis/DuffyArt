301f8d8f3467188e8a49daa328e001de5b21c03d
gnomiostasis <catsarecows@gmail.com>
2018-10-31 21:26:22 -0700

    Better gallery
    Gallery now is smaller image tiles and clickable modal images

---
+++ gallery/js/gallery.js
@@ -0,0 +1,22 @@
+﻿$(function () {
+    // Get the modal
+    //var modal = document.getElementById('myModal');
+    var modaljq = $("#myModal");
+
+    // Get the image and insert it inside the modal - use its "alt" text as a caption
+    
+    var modalimgjq = $("#img01");
+   // var captionjq = $("#caption");
+    $("#background_gallery img").click(function () {
+        modaljq.show();
+        modalimgjq.attr({ "src": $(this).attr("src") });
+        //captionjq.html($(this).attr("alt"));
+    });
+  
+
+    // Get the <span> element that closes the modal
+    $(".close").click(function () {
+        modaljq.hide();
+    });
+
+});
